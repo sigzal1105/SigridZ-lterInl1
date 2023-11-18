7f8863ff1f5a040df776efd999e300d4789ae9bb
sigzal1105 <sigzal1105@skola.goteborg.se>
2023-11-18 16:45:46 +0100

    Added run_game.sh file and added script to it.

---
+++ run_game.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Sigrid_Zälter's program"
+mkdir Sigrid_Zälter_labb
+cp *.java Sigrid_Zälter_labb/
+cd Sigrid_Zälter_labb/
+currentDir=$(pwd)
+echo "Running game from $currentDir"
+echo "compiling..."
+javac GuessingGame.java
+echo "running..."
+java GuessingGame
+echo "Done!"
+echo "Removing class files..."
+rm *.class
+ls
